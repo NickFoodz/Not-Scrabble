4e950d4a5a3411be46864ae48516abe7542a0399
NickF <npfuda@gmail.com>
2024-11-26 19:50:12 -0500

    Added a few additional tiles to XML.txt, and added a test to show the XML import works

---
+++ src/BoardTest.java
@@ -0,0 +1,19 @@
+import org.junit.Test;
+
+import java.io.FileNotFoundException;
+
+import static org.junit.Assert.*;
+
+public class BoardTest {
+
+    @Test
+    public void importCustomBoardXML() throws FileNotFoundException {
+        Position h8 = new Position(7,7);
+        //System.out.println(h8);
+        Board board1 = new Board();
+        //System.out.println(board1.getPremiumPositions());
+        Board board = new Board("XML.txt");
+        System.out.println(board.getPremiumPositions());
+
+    }
+}
