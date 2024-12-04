bc1f621c4234732cde3cc93b6a413dfcdcbce1b9
randrew <randrew291@icloud.com>
2024-12-04 10:17:50 -0500

    changed back to 2 blanks

--- src/Bag.java
+++ src/Bag.java
@@ -80,7 +80,7 @@ public class Bag implements Serializable {
         letterFrequencies.put('Q', 1);
         letterFrequencies.put('Z', 1);
         //blank tile
-        letterFrequencies.put(' ', 100);
+        letterFrequencies.put(' ', 2);
     }
 
     /**
